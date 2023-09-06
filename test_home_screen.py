91beb12870728e1939db77d6d31571ce48af282d
dgotlieb <Dgotlieb8@gmail.com>
2023-09-06 09:01:40 +0300

    Update test_home_screen.py

--- test_home_screen.py
+++ test_home_screen.py
@@ -1,20 +1,9 @@
-import pytest
+import unittest
 from selenium import webdriver
 from pages.home_page import Homepage
 from pages.logged_in_home_page import LoggedInHomePage
 
-
-def test_home_screen_flow():
-    driver = webdriver.Chrome()
-    home_page = Homepage(driver)
-    home_page.go_to_home_page()
-    home_page.click_sign_in_button()
-    logged_in_home_page = LoggedInHomePage(driver)
-    logged_in_home_page.assert_logged_in_home_page()
-    driver.quit()
-
-
-class Homepage:
+class Homepage(unittest.TestCase):
     def __init__(self, driver):
         self.driver = driver
 
@@ -24,7 +13,7 @@ class Homepage:
     def click_sign_in_button(self):
         self.driver.find_element_by_css_selector("<li class='notSigned' data-ember-action='1012'>")
 
-class LoggedInHomePage:
+class LoggedInHomePage(unittest.TestCase):
     def __init__(self, driver):
         self.driver = driver
 
@@ -33,6 +22,11 @@ class LoggedInHomePage:
             "<button id='ember1921' type='submit' gtm='כניסה ל-BUYME' class='ember-view bm-btn no-reverse main md "
             "stretch' uabtn='true'><span class='label' andiallelmwithtext='14'>כניסה ל-BUYME</span></button>")
 
+driver = webdriver.Chrome()
+home_page = Homepage(driver)
+home_page.go_to_home_page()
+home_page.click_sign_in_button()
+logged_in_home_page = LoggedInHomePage(driver)
+logged_in_home_page.assert_logged_in_home_page()
+driver.quit()
 
-if __name__ == "__main__":
-    pytest.main()
